97f0f5c6a83445fb8a3b2f2fe2a5573f18aa124a
Toni Steyskal <toni.steyskal@gmail.com>
2019-01-30 21:04:37 +0100

    Sprites pivot fixed InputManager added

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using UnityEngine;
+
+[Serializable]
+public enum Direction
+{
+	Up,
+	Down,
+	Left,
+	Right
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	[Header("Input Key Codes")]
+	public List<KeyCode> UpKeyCodes;
+	public List<KeyCode> DownKeyCodes;
+	public List<KeyCode> LeftKeyCodes;
+	public List<KeyCode> RightKeyCodes;
+
+	public CustomUnityEvent<Direction> OnInputReceived = new CustomUnityEvent<Direction>();
+
+	private void Awake()
+	{
+		OnInputReceived.AddListener (OnInputReceivedListener);
+	}
+
+	private void Update()
+	{
+		CheckKeyboardInput ();	
+	}
+
+	private bool IsOneKeyDown(List<KeyCode> keyCodes)
+	{
+		foreach (KeyCode keyCode in keyCodes)
+			if(Input.GetKeyDown(keyCode))
+				return true;
+
+		return false;
+	}
+
+	private void CheckKeyboardInput()
+	{
+		if (IsOneKeyDown (UpKeyCodes))
+			OnInputReceived.Invoke (Direction.Up);
+		else if (IsOneKeyDown (DownKeyCodes))
+			OnInputReceived.Invoke (Direction.Down);
+		else if (IsOneKeyDown (LeftKeyCodes))
+			OnInputReceived.Invoke (Direction.Left);
+		else if (IsOneKeyDown (RightKeyCodes))
+			OnInputReceived.Invoke (Direction.Right);
+	}
+
+	private void OnInputReceivedListener(Direction direction)
+	{
+		Debug.Log (direction);
+	}
+}
